9a7ef0a5669657ac5eaaf013c45277a79f4c5860
Ivan <ivan.dadalto@gmail.com>
2022-07-21 23:52:08 -0300

    resolve ex 1 de for

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,23 +1,34 @@
-// PRIMEIROS EX.
+// // PRIMEIROS EX. (ARRAY)
 
-//1.
+// //1.
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
-console.log(menuServices);
+// console.log(menuServices);
 
 
-//2.
+// //2.
 
 let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu2.indexOf('Serviços')
 
-console.log(indexOfPortfolio);
+// console.log(indexOfPortfolio);
 
-//3.
+// //3.
 
 let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu3.push('Contato')
 
-console.log(menu3);
+// console.log(menu3);
+
+// (FOR)
+
+//1. 
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1){
+
+//console.log(groceryList[index])
+}
