3fc2639c05ac98f99999de1b25fc6b0eed5b73e5
Ivan <ivan.dadalto@gmail.com>
2022-07-21 23:23:58 -0300

    resolve ex 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,16 @@
+// PRIMEIROS EX.
+
+//1.
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+//2.
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = indexOf('Portfólio')
+
+console.log(indexOfPortfolio);
