a159ea4303d42ed945c5b874e9de5facfd4861eb
Ivan <ivan.dadalto@gmail.com>
2022-07-22 13:11:22 -0300

    resolve ex 9 pós aula

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercise9.js
@@ -0,0 +1,11 @@
+let array = []
+
+for(let i = 1;i <= 25;i += 1){
+  array.push(i)
+}
+
+// console.log(array)
+
+for(let index = 0; index < array.lenght; index += 1){
+  console.log(array[index] / 2);
+}
