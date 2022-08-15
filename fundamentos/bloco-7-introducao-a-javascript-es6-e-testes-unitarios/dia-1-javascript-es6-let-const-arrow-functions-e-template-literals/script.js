66ee0ddea46a317abf2fc841d5f8a2f838ce4617
Ivan <ivan.dadalto@gmail.com>
2022-08-15 16:12:01 -0300

    Exercicio 1 utilizando operador ternário

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -39,4 +39,21 @@ const sortHof = (array) => {
   oddsAndEvens2.sort((a, b) => a - b);
 }
 sortHof();
-console.log(oddsAndEvens2)
+console.log(oddsAndEvens2);
+
+// PARTE II :
+// 1 :
+
+const factorial = (number) => {
+  let result = number;
+  for (let i = 1; i < number; i += 1) {
+  result *= i;
+  }
+  return `Esse é o fatorial ${result}`;
+}
+console.log(factorial(4))
+
+// BONUS :
+
+const factorial2 = number => number > 1 ? number * factorial2(number - 1) : 1;
+console.log(factorial2(4))
