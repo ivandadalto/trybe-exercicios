511b0c0f90b01ca4e96fe298534757a902af1c20
Ivan <ivan.dadalto@gmail.com>
2022-08-16 11:35:50 -0300

    resolve exercicios 3 e 4

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercises.js
@@ -0,0 +1,73 @@
+ // EX 1 :
+
+const testingScope = (escopo) => { 
+  if (escopo === true) {
+    let ifScope = `Não devo ser utilizada fora do meu escopo`;
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else { 
+  const elseScope = `Não devo ser utilizada fora meu escopo`;
+  console.log(elseScope);
+};
+}
+testingScope(true);
+
+// EX 2 :
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sortOddsAndEvens = (array) => {
+
+for (let i = 1 ; i <= oddsAndEvens.length - 1 ; i += 1) {
+  for (let i2 = 0; i2 < oddsAndEvens.length - 1; i2 += 1) {
+    if (oddsAndEvens[i] < oddsAndEvens[i2]) {
+      let position = oddsAndEvens[i];
+      oddsAndEvens[i] = oddsAndEvens[i2]
+      oddsAndEvens[i2] = position;
+    }
+  }
+}
+return array;
+};
+sortOddsAndEvens();
+console.log(oddsAndEvens);
+
+// BONUS 
+
+const oddsAndEvens2 = [13, 3, 4, 10, 7, 2];
+
+const sortHof = (array) => {
+  oddsAndEvens2.sort((a, b) => a - b);
+}
+sortHof();
+console.log(oddsAndEvens2);
+
+// PARTE II :
+// 1 :
+
+const factorial = (number) => {
+  let result = number;
+  for (let i = 1; i < number; i += 1) {
+  result *= i;
+  }
+  return `Esse é o fatorial ${result}`;
+}
+console.log(factorial(4))
+
+// BONUS :
+
+const factorial2 = number => number > 1 ? number * factorial2(number - 1) : 1;
+console.log(factorial2(4))
+
+// EX 2 :
+const longestWord = (param) => {
+  const wordArray = param.split(' ');
+  let maxLength = 0;
+  let result = "";
+  for (let i = 0; i <= wordArray.length - 1; i += 1) {
+  if (wordArray[i].length > maxLength) {
+  maxLength = wordArray[i].length
+  result = wordArray[i];}
+  }
+  return result;
+};
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'))
